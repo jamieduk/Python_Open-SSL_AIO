d4a13ecb31d0489c00dd87c0444e0368c2ebc650
Jay <jamied_uk@hotmail.com>
2023-04-06 22:35:20 +0100

    Add files via upload

--- aio.py
+++ aio.py
@@ -7,75 +7,58 @@
 import os
 import subprocess
 
-print("Python3 Open-SSL Crpto (c) J~Net 2023")
-
-def generate_key_pair():
-    # Generate key pair
-    subprocess.run(["openssl", "genpkey", "-algorithm", "RSA", "-out", "private_key.pem"])
-    subprocess.run(["openssl", "pkey", "-in", "private_key.pem", "-out", "key.txt", "-pubout"])
-    with open("key.txt", "rb") as f:
-        public_key=f.read()
-    with open("private_key.pem", "rb") as f:
-        private_key=f.read()
-    print("Key pair generated.")
+def generate_key():
+    if os.path.exists("private_key.txt") or os.path.exists("public_key.txt"):
+        print("Keys already exist.")
+        return
+    private_key=subprocess.check_output(["openssl", "genpkey", "-algorithm", "RSA", "-out", "private_key.txt"])
+    public_key=subprocess.check_output(["openssl", "pkey", "-in", "private_key.txt", "-pubout", "-out", "public_key.txt"])
+    print("Keys generated successfully.")
 
 def encrypt_message():
-    # Read message or prompt user for input
-    if os.path.exists("message.txt"):
-        with open("message.txt", "rb") as f:
-            message=f.read()
-    else:
-        message=input("Enter message to encrypt: ").encode()
-
-    # Encrypt message
-    if message:
-        subprocess.run(["openssl", "rsautl", "-encrypt", "-pubin", "-inkey", "key.txt", "-in", "-", "-out", "encrypted.txt"], input=message)
-        print("Message encrypted.")
-    else:
-        print("No message to encrypt.")
+    if not os.path.exists("public_key.txt"):
+        print("Public key not found. Please generate a key first.")
+        return
+    if not os.path.exists("message.txt"):
+        message=input("Enter the message to encrypt: ")
+        with open("message.txt", "w") as f:
+            f.write(message)
+    try:
+        encrypted=subprocess.check_output(["openssl", "rsautl", "-encrypt", "-pubin", "-inkey", "public_key.txt", "-in", "message.txt"])
+        with open("encrypted.txt", "wb") as f:
+            f.write(encrypted)
+        print("Message encrypted successfully.")
+    except subprocess.CalledProcessError:
+        print("Unable to encrypt message")
 
 def decrypt_message():
-    # Decrypt message
-    if os.path.exists("encrypted.txt"):
-        subprocess.run(["openssl", "rsautl", "-decrypt", "-inkey", "private_key.pem", "-in", "encrypted.txt", "-out", "decrypted.txt"])
-        print("Message decrypted.")
-    else:
-        print("Can't open encrypted.txt for reading, No such file or directory.")
+    if not os.path.exists("private_key.txt"):
+        print("Private key not found. Please generate a key first.")
+        return
+    if not os.path.exists("encrypted.txt"):
+        print("Encrypted message not found.")
+        return
+    decrypted=subprocess.check_output(["openssl", "rsautl", "-decrypt", "-inkey", "private_key.txt", "-in", "encrypted.txt"])
+    print("Decrypted message: ", decrypted.decode())
 
-# Check if key pair exists
-if os.path.exists("key.txt"):
-    with open("key.txt", "rb") as f:
-        private_key=f.read()
-        public_key=subprocess.check_output(["openssl", "pkey", "-in", "key.txt", "-pubout"])
-else:
-    generate_key_pair()
 
-# Display menu
-while True:
-    print("Menu:")
-    print("1. Generate Key Pair")
-    print("2. Encrypt Message")
-    print("3. Decrypt Message")
-    print("4. Exit")
-    choice=input("Enter your choice: ")
+def menu():
+    print("\nPython3 Open-SSL Crypto (c) J~Net 2023\n")
+    print("1. Generate a new key")
+    print("2. Encrypt message")
+    print("3. Decrypt message")
+
+    choice=input("\nEnter choice (1/2/3): ")
+
     if choice == "1":
-        generate_key_pair()
+        generate_key()
     elif choice == "2":
         encrypt_message()
     elif choice == "3":
         decrypt_message()
-    elif choice == "4":
-        break
     else:
-        print("Invalid choice. Please try again.")
+        print("Invalid choice")
 
-# Print decrypted message
-if os.path.exists("decrypted.txt"):
-    with open("decrypted.txt", "r") as f:
-        decrypted_message=f.read()
-        print("Decrypted message:")
-        print(decrypted_message)
-else:
-    with open("decrypted.txt", "w") as f:
-        f.write("Decrypted message not available")
+if __name__ == "__main__":
+    menu()
 
