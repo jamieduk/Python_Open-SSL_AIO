401a0dec2f28516116680ae6b89267f4d5b4f4a0
Jay <jamied_uk@hotmail.com>
2023-04-07 15:31:45 +0100

    Add files via upload

--- aio.py
+++ aio.py
@@ -11,10 +11,10 @@ def generate_key():
     if os.path.exists("private_key.txt") and os.path.exists("public_key.txt"):
         os.remove("private_key.txt")
         os.remove("public_key.txt")
-        print("Old keys deleted.")
+        print("Old keys Deleted.")
     private_key=subprocess.check_output(["openssl", "genpkey", "-algorithm", "RSA", "-out", "private_key.txt"])
     public_key=subprocess.check_output(["openssl", "pkey", "-in", "private_key.txt", "-pubout", "-out", "public_key.txt"])
-    print("Keys generated successfully.")
+    print("🔑 Keys Generated Successfully.")
 
         
 
@@ -30,7 +30,7 @@ def encrypt_message():
         encrypted=subprocess.check_output(["openssl", "rsautl", "-encrypt", "-pubin", "-inkey", "public_key.txt", "-in", "message.txt"])
         with open("encrypted.txt", "wb") as f:
             f.write(encrypted)
-        print("Message encrypted successfully.")
+        print("🔏 Message encrypted successfully. Saved to encrypted.txt")
     except subprocess.CalledProcessError:
         print("Unable to encrypt message")
 
@@ -41,18 +41,18 @@ def decrypt_message():
     if not os.path.exists("encrypted.txt"):
         print("Encrypted message not found.")
         return
-    decrypted = subprocess.check_output(["openssl", "rsautl", "-decrypt", "-inkey", "private_key.txt", "-in", "encrypted.txt"])
-    decrypted_message = decrypted.decode()
+    decrypted=subprocess.check_output(["openssl", "rsautl", "-decrypt", "-inkey", "private_key.txt", "-in", "encrypted.txt"])
+    decrypted_message=decrypted.decode()
     print("Decrypted message:", decrypted_message)
     with open("decrypted.txt", "w") as f:
         f.write(decrypted_message)
-    print("Decrypted message written to decrypted.txt.")
+    print("🔓 Decrypted message written to decrypted.txt.")
 
 def menu():
     print("\nPython3 Open-SSL 115 Char Secure Crypto (c) J~Net 2023\n")
-    print("1. Generate a new key")
-    print("2. Encrypt message")
-    print("3. Decrypt message")
+    print("1. 🔑 Generate a new key")
+    print("2. 🔏 Encrypt message")
+    print("3. 🔓 Decrypt message")
 
     choice=input("\nEnter choice (1/2/3): ")
 
